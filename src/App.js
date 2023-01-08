d96e57f17903eade4a1f344b53a7551e207002d6
shubh0465 <55250438+shubh0465@users.noreply.github.com>
2023-01-08 11:07:36 +0530

    Adding world map library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import WorldMap from "react-svg-worldmap"
+// import MainComponent from './Component/MainComponent';
 
-function App() {
+function App(){
+  const data = [
+    { country: "cn", value: 1389618778 }, // china
+    { country: "in", value: 1311559204 }, // india
+    { country: "us", value: 331883986 }, // united states
+    { country: "id", value: 264935824 }, // indonesia
+    { country: "pk", value: 210797836 }, // pakistan
+    { country: "br", value: 210301591 }, // brazil
+    { country: "ng", value: 208679114 }, // nigeria
+    { country: "bd", value: 161062905 }, // bangladesh
+    { country: "ru", value: 141944641 }, // russia
+    { country: "mx", value: 127318112 }, // mexico
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WorldMap
+        color="red"
+        title="Top 10 Populous Countries"
+        value-suffix="people"
+        size="lg"
+        data={data}
+       />
     </div>
   );
 }
